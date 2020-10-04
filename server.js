798a3f7a77d6135c45fa5a53896c723b9a914019
Ferkalek <ferkalekanton@gmail.com>
2020-10-04 21:14:30 +0200

    config changes

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 app.use(cors());
 
+app.use(express.static("./dist/peachtree"));
+app.get("/", function (req, res) {
+  res.sendFile("index.html", { root: "dist/peachtree" });
+});
+
 app.get("/api/transactions", (req, res, next) => {
   fs.readFile(
     path.join(__dirname, "mock", "transactions.json"),
